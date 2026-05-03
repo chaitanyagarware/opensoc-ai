d1f27087943606a18479c387d8d58c80ce9e2f55
Chaitanya Vilas Garware <97582002+chaitanyagarware@users.noreply.github.com>
2026-05-03 13:47:18 -0500

    Update Vite config to change root and output directory

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/opensoc-ai/',
+  root: 'web',
+  build: {
+    outDir: '../dist'
+  },
+  base: '/opensoc-ai/'
 })
